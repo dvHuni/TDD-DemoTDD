5b0d019518950201ec8792076ce08c0e626c011e
AhnSangHoon <ansh0324@gmail.com>
2022-03-26 16:18:48 +0900

    test: create DataModel class

---
+++ FitNess/FitNess/DataModel/DataModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataModel.swift
+//  FitNess
+//
+//  Created by AhnSangHoon on 2022/03/26.
+//  Copyright © 2022 Razeware. All rights reserved.
+//
+
+import Foundation
+
+class DataModel {
+
+}
--- FitNess/FitNessTests/Cases/Data Model/DataModelTests.swift
+++ FitNess/FitNessTests/Cases/Data Model/DataModelTests.swift
@@ -7,8 +7,10 @@
 //
 
 import XCTest
+@testable import FitNess
 
 class DataModelTests: XCTestCase {
+  var sut: DataModel!
   
   override func setUpWithError() throws {
     // Put setup code here. This method is called before the invocation of each test method in the class.
